9086265bd171ad5b4ce5803eba3a7d6c7f28f2e7
Jisan Mahmud <jisa.mahmud20@gmail.com>
2024-09-25 16:52:26 +0600

    error and success message add in frontend

--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .forms import VoteForm
 from .models import Vote, VoteCount
+from django.contrib import messages
+
+
 def home(request):
     if request.method == 'POST':
         form = VoteForm(data= request.POST)
@@ -8,25 +11,26 @@ def home(request):
             voter_name = form.cleaned_data.get('voter_name')
             has_voter = Vote.objects.filter(voter_name__icontains = voter_name)
             if has_voter.exists():
-                vote = Vote.objects.get(voter_name__icontains = voter_name)
-                if vote.voter_count < 2:
-                    vote.participate_name = form.cleaned_data['participate_name']
-                    vote.voter_count = vote.voter_count + 1
-                    vote.save()
+                messages.error(request, 'You have already voted.')
+                # vote = Vote.objects.get(voter_name__icontains = voter_name)
+                # if vote.voter_count < 1:
+                #     vote.participate_name = form.cleaned_data['participate_name']
+                #     vote.voter_count = vote.voter_count + 1
+                #     vote.save()
 
-                    # total vote count 
-                    vote_count, created = VoteCount.objects.get_or_create(name=form.cleaned_data['participate_name'])
-                    vote_count.total_vote = Vote.objects.filter(participate_name=form.cleaned_data['participate_name']).count()
-                    vote_count.save()
-                else:
-                    print('Limit exits.')
+                #     # total vote count 
+                #     vote_count, created = VoteCount.objects.get_or_create(name=form.cleaned_data['participate_name'])
+                #     vote_count.total_vote = Vote.objects.filter(participate_name=form.cleaned_data['participate_name']).count()
+                #     vote_count.save()
+                # else:
+                #     print('Limit exits.')
 
             else:
                 print('No')
                 vote = form.save()
                 vote.voter_count = vote.voter_count + 1
                 vote.save()
-
+                messages.success(request, 'You have successfully voted.')
                  # total vote count 
                 vote_count, created = VoteCount.objects.get_or_create(name=form.cleaned_data['participate_name'])
                 vote_count.total_vote = Vote.objects.filter(participate_name=form.cleaned_data['participate_name']).count()
